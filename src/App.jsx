e96d78aa23e8ecb409f24208746452cc2a4874b8
TarunPatidar04 <tarun.coderfarm@gmail.com>
2026-03-01 19:17:21 +0530

    feat: Add `useEffect` hook to fetch current user data on application initialization.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
+import { useEffect } from "react";
+import axios from "axios";
 
 export const API_BASE_URL = "http://localhost:3000/api";
 const App = () => {
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await axios.get(`${API_BASE_URL}/user/current-user`, {
+          withCredentials: true,
+        });
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUser();
+  }, []);
   return (
     <>
       <Routes>
